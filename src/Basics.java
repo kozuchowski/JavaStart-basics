74e24208c50318c67699ec604fd98276bab86cfe
kozuchowski <marekkozuchowski4@gmail.com>
2021-03-15 18:46:02 +0100

    total basics , don't even check it out

--- src/Basics.java
+++ src/Basics.java
@@ -1,6 +1,6 @@
 public class Basics{
 	public static void javaBasics () {
-		System.out.println("Java Start!");
+		System.out.println("Marek Ko¿chowski");
 	}
 	public static void main (String[] args) {
 		javaBasics();
